69a5bc324f6f683f6209291fe91494519ce13631
pressure101 <cavamorr@gmail.com>
2022-03-01 03:12:20 -0500

    functioning end to end
    fixed all tweets endpoint, and my tweets endpoint, fixed logic for checking all users registered, and working posttweet repsonse

--- demo/src/main/java/com/tweeter/demo/TweeterController.java
+++ demo/src/main/java/com/tweeter/demo/TweeterController.java
@@ -1,5 +1,6 @@
 package com.tweeter.demo;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.persistence.EntityNotFoundException;
 
@@ -39,12 +41,21 @@ public class TweeterController {
     @PostMapping("/") 
     public String submitLogin(Model model, User user) {
         List<User> userList;
-        User loggedInUser;
+        User loggedInUser = new User();
         model.addAttribute("username", user.username);
         
         try {
+        // loop through users in db --> max 5 users
         userList = userRepo.findAll();
-        loggedInUser = userList.get(0);
+        for(User loopedUser: userList) {
+            if(loopedUser.username.equals(user.username)) {
+                loggedInUser = loopedUser;
+            }
+        }
+        // check if the above loop found a user, if not throw error page
+        if(loggedInUser.username == null){
+            return "error";
+        }
 
         System.out.println(loggedInUser.getPassword());
         System.out.println(user.getPassword());
@@ -60,25 +71,39 @@ public class TweeterController {
     }
 
     @GetMapping("/feed/all") 
-    public String getAllTweets(Model model) {
+    public ModelAndView getAllTweets(Model model) {
         // logic to grab all tweets from db
+        
+        ModelAndView mav = new ModelAndView("tweets");
+        mav.addObject("tweets", tweetsRepo.findAll());
         model.addAttribute("username", currentUsername);
         System.out.println("Hi " + currentUsername);
-        return "tweets";
+        return mav;
     }
 
     @GetMapping("/feed") 
-    public String getUserTweets(Model model) {
-        // logic to grab users tweets
+    public ModelAndView getUserTweets(Model model) {
+        ModelAndView mav = new ModelAndView("myTweets");
+        
+        List<Tweets> allTweets = tweetsRepo.findAll();
+        List<Tweets> filteredTweets = new ArrayList<Tweets>();
+        // filter out not applicable tweets
+        for(Tweets tweet: allTweets){
+            if(tweet.getUsername().equals(currentUsername)){
+                filteredTweets.add(tweet);
+            }
+        }
+        mav.addObject("filteredTweets", filteredTweets);
         model.addAttribute("username", currentUsername);
         System.out.println("Hi " + currentUsername);
-        return "myTweets";
+        return mav;
     }
 
     @PostMapping("/postTweet")
-    public String postTweet(@ModelAttribute String content, Model model) {
+    public String postTweet(String content, Model model) {
         model.addAttribute("username", currentUsername);
         // logic to insert into db
+        System.out.println(content);
         Tweets tweet =  new Tweets();
         tweet.setContent(content);
         tweet.setTimeTweeted(new Date());
